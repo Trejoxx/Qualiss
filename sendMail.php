5de4d45f9022df75ac4f27467bf0b81669d47f83
GEPP\50104511 <barrientos.isc@gmail.com>
2018-07-19 10:32:29 -0500

    Se agrego scritp PHP para envio de emails

---
+++ sendMail.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_POST['email'])) {
+
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$email_to = "jecruzt@gmail.com";
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['name']) ||
+!isset($_POST['subject']) ||
+!isset($_POST['email']) ||
+!isset($_POST['cellphone']) ||
+!isset($_POST['message'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+$email_subject = "Asunto: ".$_POST['subject'];
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['name'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Celular: " . $_POST['cellphone'] . "\n";
+$email_message .= "Comentarios: " . $_POST['message'] . "\n\n";
+
+$email_from = $_POST['email'];
+$email_subject = $email_from;
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
+?>
